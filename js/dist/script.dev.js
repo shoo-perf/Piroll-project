55af8d3c92053e91934b50ed3e1466bb2c0ca68a
Arkady Shulepov <arkadiy123440@gmail.com>
2021-05-10 20:04:30 +0300

    add new pages, slick-slider, gallery, page 'projects.html' goes from 'services' in hamburger 'index.html'

--- js/dist/script.dev.js
+++ js/dist/script.dev.js
@@ -18,4 +18,39 @@ $(window).scroll(function () {
 });
 $("img").attr({
   loading: "lazy"
+})
+/**Настройка slick-slider*/
+/ $(document).ready(function () {
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }]
+  });
 });
